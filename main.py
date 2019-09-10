03d4d0798111498be93cf36596a51f8d5d8b4be8
gomezpaula <gomhez.paula@gmail.com>
2019-09-10 14:57:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,45 +12,28 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.http import MediaFileUpload
-SCOPES = ['https://www.googleapis.com/auth']
+from requests import resp
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
+CLIENT_SECRET_FILE = 'credentials.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+authInst = auth.auth(SCOPES,CLIENT_SECRET_FILE,APPLICATION_NAME)
+credentials = authInst.getCredentials()
+http = credentials.authorize(httplib2.Http())
+drive_service = discovery.build('drive', 'v3', http=http)
 
 
 #Crear/subir un archivo
-def createFile(filename,filepath,mimetype):
-   file_metadata = {'name': filename}
-media = MediaFileUpload(filepath, 
-                        mimetype= mimetype)
-file = drive_service.files().create(body=file_metadata,
-                                    media_body=media,
-                                    fields='id').execute()
+def uploadFile(filename,filepath,mimetype):
+    file_metadata = {'name': filename}
+    media = MediaFileUpload(filepath, mimetype=mimetype)
+    file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
 
-if not createFile:
-        print('Status:404')
-    else:
-        createFile('descarga.jpg','descarga.jpg','image/jpeg')
-print ('File ID: %s' % file.get('name') % file.get('id')% 'Status:200')
+    resp = requests.get('    ')
+if resp.status_code != 200:
+    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+for t_item in resp.json():    
+    print('File ID: {} File name: {}' .format (file.get('id'), file.get('description')))
 
-
-
-#Buscar un archivo que contenga una palabra enviada por parámetro
-def searchFile():
-
-    page_token = None
-while True:
-    response = drive_service.files().list(q="mimeType='image/jpeg'",
-                                          spaces='drive',
-                                          fields='nextPageToken, files(id, name)',
-                                          pageToken=page_token).execute()
-    for file in response.get('files', []):
-       
-        print ('Found file: %s (%s)' % (file.get('name'), file.get('id')))
-    page_token = response.get('nextPageToken', None)
-    if page_token is None:
-        break
-
-if not items:
-        print('Status:404')
-    else:
-        searchFile('id='"1-9QLUIcs-g8PVxzOfoBzlF5IvrABFOYv"'','fullText contains '"API"'')
-        print('Status:200')
-        
+nombreArchivo = input("Nombre archivo: \n")
+uploadFile(nombreArchivo,'descarga.jpg','image/jpeg')
